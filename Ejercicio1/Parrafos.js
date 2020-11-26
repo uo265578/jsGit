58a72c1f082b58c3ad8ca28fe154c27d0beaf8e5
Santiago <uo265578@uniovi.es>
2020-11-26 17:28:55 +0100

    arregl ejer1

--- Ejercicio1/Parrafos.js
+++ Ejercicio1/Parrafos.js
@@ -1,9 +1,9 @@
-document.write("<p>Curso actual: ")
-document.write(asignatura.curso)
+document.write("<p>Curso actual: ");
+document.write(asignatura.curso);
 document.write("</p>");
-document.write("<p>Nombre del estudiante: ")
-document.write(asignatura.estudiante)
-document.write</"p">
-document.write("<p>E-mail: ")
-document.write(asignatura.email)
-document.write</"p">
+document.write("<p>Nombre del estudiante: ");
+document.write(asignatura.estudiante);
+document.write(</"p">);
+document.write("<p>E-mail: ");
+document.write(asignatura.email);
+document.write(</"p">);
