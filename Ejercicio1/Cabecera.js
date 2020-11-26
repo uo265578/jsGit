82f0ff0dd069412f68b9d37f4bf3cb8833f72aed
Santiago <uo265578@uniovi.es>
2020-11-26 17:38:37 +0100

    arregl ejer1

--- Ejercicio1/Cabecera.js
+++ Ejercicio1/Cabecera.js
@@ -1,14 +1,8 @@
-class Asignatura{
-	constructor(nombre, titulacion, centro, universidad, curso, estudiante, email){
-		this.nombre=nombre
-		this.titulacion=titulacion
-		this.centro=centro
-		this.universidad=universidad
-		this.curso=curso
-		this.estudiante=estudiante
-		this.email=email
-		
-	}
-}
-
-asignatura=new Asignatura("Software y Estándares para la Web", "Grado de Ingeniería Informática del Software","Escuela de Ingeniería Informática del Software", "Universidad de Oviedo", "2019-20", "Santiago Fidalgo Sallés", "uo265578@uniovi.es");
+var asignatura = new Object();
+	asignatura.nombreAsignatura = "Software y Estándares para la Web";
+	asignatura.titulacion = "Grado Ingenieria Informática del Software";
+	asignatura.centro = "Escuela de Ingeniería Informática de Oviedo";
+	asignatura.universidad = "Universidad de Oviedo";
+	asignatura.curso = "2020-2021";
+	asignatura.estudiante = "Santiago Fidalgo Sallés";
+	asignatura.email = "uo265578@uniovi.es";
